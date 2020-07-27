c4a66a5ba6d3f3ecdef3e135e375f5777343ed5e
yulianovikova <y.novikova@dodopizza.com>
2020-07-28 00:06:17 +0300

    add some Links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
+import Link from 'next/link'
+
 const Index = () => (
     <div>
         <p>Hello, world!</p>
+        <Link href="about">About</Link>
+        <br/>
+        <Link href="product/[id]" as={`/product/${1}`}>Product 1</Link>
     </div>
 );
 
