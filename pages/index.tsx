fdcdf5054b4cb5f11a4eabd8560d6441d2a879fd
yulianovikova <y.novikova@dodopizza.com>
2020-07-30 23:11:01 +0300

    minor fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,11 @@ import Link from 'next/link'
 const Index = () => (
     <div>
         <p>Hello, world!</p>
-        <Link href="about">About</Link>
+        <Link href="about"><a>About</a></Link>
         <br/>
-        <Link href="product/[id]" as={`/product/${1}`}>Product 1</Link>
+        <Link href="product/[id]" as={`/product/${1}`}><a>Product 1</a></Link>
+        <br/>
+        <Link href="product/[id]" as={`/product/${2}`}><a>Product 2</a></Link>
     </div>
 );
 
